8f1d70355fcf5fabc4c0140b4b8c30da70017137
XLpacman805 <XLpacman805@gmail.com>
2015-05-31 18:21:39 -0700

    The wear app will now use voice recognition to get the user's speech. Next: Store it as a string variable.

--- wear/src/main/java/com/appscalibur/wristtweetalpha09/MainActivityWear.java
+++ wear/src/main/java/com/appscalibur/wristtweetalpha09/MainActivityWear.java
@@ -1,13 +1,40 @@
 package com.appscalibur.wristtweetalpha09;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivityWear extends Activity {
 
     private TextView mTextView;
+
+    //VOICE RECOGNITION STUFF
+    private static final int SPEECH_REQUEST_CODE = 0;
+    //Create an intent that can start the speech recognizer activity
+    private  void displaySpeechRecognizer(){
+        Intent speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        // Start the activity, the intent will be populated with the speech text
+        startActivityForResult(speechIntent, SPEECH_REQUEST_CODE);
+    }
+    // This callback is invoked when the Speech Recognizer returns.
+    // This is where you process the intent and extract the speech text from the intent.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            // Do something with spokenText
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,4 +48,11 @@ public class MainActivityWear extends Activity {
             }
         });
     }
+
+    //Switch to round voice activity
+    public void openRoundVoiceActivity(View view){
+        displaySpeechRecognizer();
+    }
+
 }
+
